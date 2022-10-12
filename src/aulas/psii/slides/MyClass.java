6116ab7098cf532cd8300d8813ef38c06621f6ae
Marcos Lauder <the_lauder@hotmail.com>
2022-10-11 21:04:21 -0300

    adicionando exemplos do slide

---
+++ src/aulas/psii/slides/MyClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulas.psii.slides;
+
+/**
+ *
+ * @author Lauder
+ */
+public class MyClass {
+    public static void main(String[] args) {
+        Casa casa1 = new Casa();
+        Casa casa2 = new Casa();
+        Casa casa3 = new Casa();
+        
+        casa1.corDaParede = "Branca";
+        casa2.corDaParede = "Azul";
+        
+        casa1.ligarLuzDaCozinha();
+    }
+}
